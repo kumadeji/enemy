6bf7b4e1e406cc44eadab3e7a5eaad39d903eed7
kumadeji <87421331+kumadeji@users.noreply.github.com>
2026-08-03 18:31:25 +0300

    test

--- src/pages/TempTable.jsx
+++ src/pages/TempTable.jsx
@@ -3,13 +3,14 @@ export default function Media() {
     <main className="container">
       <h1>Таблица: проблема с никами и регистрациями в клане</h1>
       <div className="card">
-<iframe
-  src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRHphlLIK6i0m2PMjXfXDfQJUq2GKilASFWJ6wrqGSJFyNTy6G3VwrwGGo1Q5WWNGK9AQ9ph-lrjbF-/pubhtml?gid=1027293757&single=true&widget=false&headers=false&range=A1:J28"
-  width="100%"
-  height="900"
-  style="border: 0;"
-></iframe>
-	  </div>
+        <iframe
+          src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRHphlLIK6i0m2PMjXfXDfQJUq2GKilASFWJ6wrqGSJFyNTy6G3VwrwGGo1Q5WWNGK9AQ9ph-lrjbF-/pubhtml?gid=1027293757&single=true&widget=false&headers=false&range=A1:J28"
+          width="100%"
+          height="900"
+          frameBorder="0"
+          style={{ border: "none" }}
+        ></iframe>
+      </div>
     </main>
   );
 }
