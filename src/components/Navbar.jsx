240cfab3cf7cada97bbb119cbb298a815d5d5526
kumadeji <87421331+kumadeji@users.noreply.github.com>
2026-08-09 23:45:12 +0300

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,10 +26,6 @@ export default function Navbar() {
         <nav>
           <NavLink to="/" end className={linkClass}>Главная</NavLink>
           <NavLink to="/roster" className={linkClass}>Состав</NavLink>
-          // <NavLink to="/media" className={linkClass}>Видео</NavLink>
-          // <NavLink to="/charter" className={linkClass}>Устав и манифест</NavLink>
-          // <NavLink to="/history" className={linkClass}>История</NavLink>
-          // <NavLink to="/contact" className={linkClass}>Контакты</NavLink>
 
           {canSeeArmaHQ && (
             <>
@@ -56,3 +52,8 @@ export default function Navbar() {
     </header>
   );
 }
+
+// <NavLink to="/media" className={linkClass}>Видео</NavLink>
+// <NavLink to="/charter" className={linkClass}>Устав и манифест</NavLink>
+// <NavLink to="/history" className={linkClass}>История</NavLink>
+// <NavLink to="/contact" className={linkClass}>Контакты</NavLink>
