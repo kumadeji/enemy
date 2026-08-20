7e09c9607cf9e103d996765b8bad98ded5ec6472
kumadeji <87421331+kumadeji@users.noreply.github.com>
2026-08-21 01:14:10 +0300

    test

--- src/pages/ArmaHQ.jsx
+++ src/pages/ArmaHQ.jsx
@@ -4,7 +4,7 @@ export default function ArmaHQ() {
   return (
     <main className="container">
       <h1>Штаб Arma Reforger</h1>
-      <p className="page-lead">Раздел для навигации по инструментам направления Arma Reforger. Пока доступно только два инструкция, но раздел будет наполняться.</p>
+      <p className="page-lead">Раздел для навигации по инструментам направления Arma Reforger. Пока доступно только два инструмента, но раздел будет наполняться.</p>
       <div className="card">
         <Link to="/hq/arma/stats" className="btn hq-primary-btn">Клановая статистика</Link>
 		<Link to="/queue" className="btn hq-primary-btn">Очередь на командира отделения</Link>
