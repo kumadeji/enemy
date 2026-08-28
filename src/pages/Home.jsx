d2cd9c55542317d55ab4050f0b83845acf038db2
kumadeji <87421331+kumadeji@users.noreply.github.com>
2026-08-28 11:45:34 +0300

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,6 +19,17 @@ export default function Home() {
       </section>
 	  
 	  <section className="card">
+        <h2>Видео</h2>
+		<p className="hint">Трейлер нашего клана в Arma Reforger:</p>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/L372YQEQsWU?si=FqY47fCF6WQxpDaV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+		<br/>
+		<p className="hint">Как вступить в клан и начать играть с нами:</p>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/bUZEPOiWbsE?si=g-4h_rtEM816YtFz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+		<br/>
+		<p className="hint">Ещё больше видео от наших соклановцев — в секции <Link to="/media">«Видео»</Link>!</p>
+	  </section>
+	  
+	  <section className="card">
         <h2>Общение</h2>
 		<p className="hint">Регистрация в сообществе происходит на этом сайте, но мы общаемся в Discord!</p>
 		<DiscordWidget />
